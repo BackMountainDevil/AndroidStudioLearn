db9fc15e7e404e5f445bdac69d29ea242d38cab3
Kearney <kearneyyi@cau.edu.cn>
2020-08-31 21:47:43 +0800

    新加了屏幕像素尺寸的单位

--- Chapter2/ScreenPix/app/src/main/java/com/example/screenpix/MainActivity.java
+++ Chapter2/ScreenPix/app/src/main/java/com/example/screenpix/MainActivity.java
@@ -22,21 +22,19 @@ public class MainActivity extends AppCompatActivity {
         int width = wm1.getDefaultDisplay().getWidth();
         int height = wm1.getDefaultDisplay().getHeight();*/
 
-
         WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);  //获取屏幕参数方法1
         DisplayMetrics dm = new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(dm);
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-        float density = dm.density;
-
+        float density = dm.density; //density：像素密度，即一个dp单位包含多少个px单位。
 
         TextView tv_width = findViewById(R.id.tv_width);
         TextView tv_height = findViewById(R.id.tv_height);
         TextView tv_density = findViewById(R.id.tv_density);
 
-        tv_width.setText("Width： "+Integer.toString(width));
-        tv_height.setText("Height： "+Integer.toString(height));
-        tv_density.setText("Density :"+Float.toString(density));
+        tv_width.setText("Width： "+Integer.toString(width)+" px");
+        tv_height.setText("Height： "+Integer.toString(height)+" px");
+        tv_density.setText("Density : "+Float.toString(density));
     }
 }
