0952bba8966461b76bfe925ddad0356aad0257ea
Kearney <kearneyyi@cau.edu.cn>
2020-09-02 09:45:36 +0800

    Avtivity生命活动Log演示demo

---
+++ Chapter3/Activities/app/src/main/java/com/example/activities/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity  {
+    String msg = "Main";   //日志Verbose   msg  Main
+
+    /** 当活动第一次被创建时调用 */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+    }
+
+    /** 当活动即将可见时调用 */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** 当活动可见时调用 */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** 当其他活动获得焦点时调用 */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** 当活动不再可见时调用 */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** 当活动将被销毁时调用 */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+
+
+
+}
