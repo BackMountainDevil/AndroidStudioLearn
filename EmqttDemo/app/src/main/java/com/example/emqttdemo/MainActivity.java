5d7501b3fdb4b77e2bfe4a61d0f33e5b91299ed7
Kearney <kearneyyi@cau.edu.cn>
2020-09-07 11:13:41 +0800

    实现成功连接测试服务器

---
+++ EmqttDemo/app/src/main/java/com/example/emqttdemo/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.emqttdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+
+public class MainActivity extends AppCompatActivity {
+                   //创建一个mqtt_client对象
+    String subTopic = "testtopic/#";
+    String pubTopic = "testtopic/1";
+    String content = "Hello World";
+    int qos = 2;
+    String broker = "tcp://broker.emqx.io:1883";        //"tcp://broker.emqx.io:1883" 测试成功
+    String clientId = "temp";
+    MemoryPersistence persistence = new MemoryPersistence();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mqtt();
+    }
+
+
+    private void mqtt()
+    {
+        try {
+            MqttClient client = new MqttClient(broker, clientId, persistence);
+
+            // MQTT 连接选项
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setUserName("temp");
+            connOpts.setPassword("temp".toCharArray());
+            // 保留会话
+            connOpts.setCleanSession(true);
+
+            // 设置回调
+            //client.setCallback(new PushCallback());
+
+            // 建立连接
+            System.out.println("Connecting to broker: " + broker);
+            client.connect(connOpts);
+
+            System.out.println("Connected");
+            System.out.println("Publishing message: " + content);
+
+            // 订阅
+            client.subscribe(subTopic);
+
+            // 消息发布所需参数
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            client.publish(pubTopic, message);
+            System.out.println("Message published");
+
+            client.disconnect();
+            System.out.println("Disconnected");
+            client.close();
+            System.exit(0);
+        } catch (MqttException me) {
+            System.out.println("reason " + me.getReasonCode());
+            System.out.println("msg " + me.getMessage());
+            System.out.println("loc " + me.getLocalizedMessage());
+            System.out.println("cause " + me.getCause());
+            System.out.println("excep " + me);
+            me.printStackTrace();
+        }
+
+    }
+
+}
